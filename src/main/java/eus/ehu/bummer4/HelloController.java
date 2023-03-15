567094d9d327ae3811b42c3d6b9dfa7fb3e905e4
Daria <dpaslavska001@ikasle.ehu.eus>
2023-03-15 22:22:56 +0300

    Bummer4

--- src/main/java/eus/ehu/bummer4/HelloController.java
+++ src/main/java/eus/ehu/bummer4/HelloController.java
@@ -6,6 +6,7 @@ import com.google.gson.JsonObject;
 import com.google.gson.reflect.TypeToken;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.web.WebEngine;
@@ -37,48 +38,109 @@ public class HelloController {
     @FXML
     private WebView tootView;
 
+
+    @FXML
+    private CheckBox boosted_;
+
     private int index;
+    private int index_not_boosted;
+
     private List<Status> toots;
 
     @FXML
     void nextButton(ActionEvent event) {
-        if (index != toots.size() - 1) {
-            index++;
+
+        if (!boosted_.isSelected()) {
+            index_not_boosted = getNextMineToot();
+            index = index_not_boosted;
+            ActionMine();
         }
-        Action();
+        else {
+            if (index != toots.size() - 1) {
+                index++;
+            }
+            Action();
+        }
 
     }
 
     @FXML
     void previousButton(ActionEvent event) {
-        if (index != 0) {
-            index--;
+
+        if (!boosted_.isSelected()) {
+            index_not_boosted = getPreviousMineToot();
+            index = index_not_boosted;
+            ActionMine();
         }
-        Action();
+        else {
+            if (index != 0) {
+                index--;
+            }
+            Action();
+        }
+
+
     }
+
     @FXML
     void initialize() {
 
+
         String id = "109897302633891405";
         index = 0;
+        index_not_boosted = 0;
         //String id = "109842111446764244";
         Gson gson = new Gson();
-        String body = Utils.request("accounts/"+id+"/statuses");
+        String body = Utils.request("accounts/" + id + "/statuses");
         JsonArray jsonArray = gson.fromJson(body, JsonArray.class);
         Type statusList = new TypeToken<ArrayList<Status>>() {}.getType();
         toots = gson.fromJson(jsonArray.getAsJsonArray(), statusList);
-        Action();
+        index_not_boosted = getNextMineToot();
+        ActionMine();
     }
+
     void Action() {
+
         dateText.setText(toots.get(index).created_at);
-        if ( toots.get(index).content.isEmpty() ) {
+        if (toots.get(index).content.isEmpty()) {
             authorName.setText(toots.get(index).reblog.account.username);
             tootView.getEngine().loadContent(toots.get(index).reblog.content);
-        }
-        else {
+        } else {
             authorName.setText(toots.get(index).account.username);
             tootView.getEngine().loadContent(toots.get(index).content);
         }
+
+    }
+
+    void ActionMine() {
+
+        dateText.setText(toots.get(index_not_boosted).created_at);
+        if (toots.get(index_not_boosted).content.isEmpty()) {
+            authorName.setText(toots.get(index_not_boosted).reblog.account.username);
+            tootView.getEngine().loadContent(toots.get(index_not_boosted).reblog.content);
+        } else {
+            authorName.setText(toots.get(index).account.username);
+            tootView.getEngine().loadContent(toots.get(index_not_boosted).content);
+        }
+
+    }
+
+    int getNextMineToot() {
+        for (int i = index_not_boosted + 1; i < toots.size(); i++) {
+            if (!toots.get(i).content.isEmpty()) {
+                return i;
+            }
+        }
+        return index_not_boosted;
+    }
+
+    int getPreviousMineToot() {
+        for (int i = index_not_boosted - 1; i >= 0; i--) {
+            if (!toots.get(i).content.isEmpty()) {
+                return i;
+            }
+        }
+        return index_not_boosted;
     }
 
 }
